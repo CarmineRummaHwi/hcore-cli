76fb8020a35e0805b388c82698b3c4018af461f9
hwicarminerumma <carmine.rumma@healthwareinternational.com>
2019-12-09 14:14:06 +0100

    Fix.

--- src/index.php
+++ src/index.php
@@ -5,4 +5,5 @@ $cwd = empty($_SERVER['PWD']) ? getcwd() : $_SERVER['PWD'];
 require_once (dirname(__FILE__) . "/autoload.php");
 require_once (dirname(__FILE__) . "/bootstrap.php");
 
+
 HCli::getInstance()->call("apitests init");
